7dde6c7ac68b6ee5b1df85c501b6af3af8242e83
Faysal2123 <foysal2005n1@gmail.com>
2024-09-19 22:16:29 +0600

    login section

---
+++ js/login.js
@@ -0,0 +1,16 @@
+document.getElementById('btn-login').addEventListener('click',function(event){
+    event.preventDefault();
+    console.log('login button clicked')
+
+    const phoneNumber=document.getElementById('phone-number').value;
+    const pinNumber=document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
+
+    if(phoneNumber==='5' && pinNumber==='1234'){
+        console.log('you are logged in')
+        window.location.href='home.html';
+    }
+    else{
+        alert('wrong phone number or pin')
+    }
+})
